4ac6e5a72e1932627763468d301c431594c0bb5c
Allam Taju Sarof <55141556+Allam0053@users.noreply.github.com>
2023-01-24 21:58:00 +0700

    chore: update deps

--- tailwind.config.js
+++ tailwind.config.js
@@ -166,7 +166,7 @@ module.exports = {
         {
           [`@media (min-width: ${screens['2xl']})`]: {
             '.container': {
-              'max-width': '1280px',
+              'max-width': '1400px',
             },
           },
         },
