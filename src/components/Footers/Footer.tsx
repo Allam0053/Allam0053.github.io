6e1f2c5727c8d3dbccefeeb1135bebca7d8911d5
Allam Taju Sarof <55141556+Allam0053@users.noreply.github.com>
2023-02-01 21:33:41 +0700

    fix: deps and eslint warnings

--- src/components/Footers/Footer.tsx
+++ src/components/Footers/Footer.tsx
@@ -82,12 +82,12 @@ export default function Footer() {
                       </UnstyledLink>
                     </li>
                     <li>
-                      <a
+                      <UnstyledLink
                         className='block pb-2 text-sm font-semibold text-slate-600 hover:text-slate-800'
                         href='/blogs/'
                       >
                         Blog
-                      </a>
+                      </UnstyledLink>
                     </li>
                     <li>
                       <a
