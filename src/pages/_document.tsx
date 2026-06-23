712b3a32cf1faf3339accc5a6a28f5bb1a5cc4de
Allam Taju Sarof <allam.taju@samsung.com>
2026-06-24 01:18:54 +0700

    feat: add google analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -24,6 +24,18 @@ class _Document extends Document {
             crossOrigin='anonymous'
           />
           <link rel='manifest' href='/manifest.json' />
+
+          <script async src='https://www.googletagmanager.com/gtag/js?id=G-EVVPR6LFF7'></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag() { dataLayer.push(arguments); }
+                gtag('js', new Date());
+                gtag('config', 'G-EVVPR6LFF7');
+              `,
+            }}
+          />
         </Head>
         <body className='w-screen' style={{ overflowX: 'hidden' }}>
           <Main />
